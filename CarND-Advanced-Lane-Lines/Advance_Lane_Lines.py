e46951d8401820162621df2907491d8ad059b721
t-f-g <tfgroth@gmail.com>
2017-02-07 13:13:28 -0500

    Added 2nd order polynomial fitting

--- CarND-Advanced-Lane-Lines/Advance_Lane_Lines.py
+++ CarND-Advanced-Lane-Lines/Advance_Lane_Lines.py
@@ -14,7 +14,7 @@
 # 
 # ## First, I'll compute the camera calibration using chessboard images
 
-# In[22]:
+# In[2]:
 
 import numpy as np
 import cv2
@@ -54,7 +54,7 @@ for fname in images:
 #cv2.destroyAllWindows()
 
 
-# In[4]:
+# In[3]:
 
 import pickle
 get_ipython().magic('matplotlib inline')
@@ -83,7 +83,7 @@ ax2.imshow(dst)
 ax2.set_title('Undistorted Image', fontsize=30)
 
 
-# In[69]:
+# In[4]:
 
 # Test undistortion on a lane image
 img_str8 = cv2.imread('test_images/straight_lines1.jpg')
@@ -108,7 +108,7 @@ ax2.imshow(cv2.cvtColor(img_test5_undistorted, cv2.COLOR_BGR2RGB))
 ax2.set_title('Undistorted test5.jpg', fontsize=20)
 
 
-# In[70]:
+# In[9]:
 
 def warper(img, src, dst):
 
@@ -120,7 +120,7 @@ def warper(img, src, dst):
     return warped
 
 
-# In[73]:
+# In[10]:
 
 src = np.float32(
     [[(img_size[0] / 2) - 55, img_size[1] / 2 + 100],
@@ -146,21 +146,26 @@ ax2.imshow(img_test5_warped)
 ax2.set_title('Warped test5.jpg', fontsize=20)
 
 
+# The results above show us that our perspective warp is working fine.
+
 # ## The next step is to prepare the image images before edge detection, focusing on yellow and white lines
 
-# In[77]:
+# In[13]:
 
 # test a few images first
 test5_HLS_img = cv2.cvtColor(img_test5_warped, cv2.COLOR_RGB2HLS).astype(np.float)
 
 # Visualize HSL
-f, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=(20,10))
+f, (ax0, ax1, ax2, ax3) = plt.subplots(1, 4, figsize=(20,10))
 ax0.imshow(test5_HLS_img[:,:,0], cmap='gray')
 ax0.set_title('test5 Hue', fontsize=20)
 ax1.imshow(test5_HLS_img[:,:,1], cmap='gray')
 ax1.set_title('test5 Lightness', fontsize=20)
 ax2.imshow(test5_HLS_img[:,:,2], cmap='gray')
 ax2.set_title('test5 Saturation', fontsize=20)
+gray = cv2.cvtColor(img_test5_warped, cv2.COLOR_RGB2GRAY)
+ax3.imshow(gray, cmap='gray')
+ax3.set_title('test5 gray', fontsize=20)
 
 test5_H_sobelx = cv2.Sobel(test5_HLS_img[:,:,0], cv2.CV_64F, 1, 0, ksize = 5)
 abs_test5_H_sobelx = np.absolute(test5_H_sobelx) # Absolute x derivative to accentuate lines away from horizontal
@@ -171,35 +176,44 @@ scaled_test5_L_sobelx = np.uint8(255*abs_test5_L_sobelx/np.max(abs_test5_L_sobel
 test5_S_sobelx = cv2.Sobel(test5_HLS_img[:,:,2], cv2.CV_64F, 1, 0, ksize = 5)
 abs_test5_S_sobelx = np.absolute(test5_S_sobelx) # Absolute x derivative to accentuate lines away from horizontal
 scaled_test5_S_sobelx = np.uint8(255*abs_test5_S_sobelx/np.max(abs_test5_S_sobelx))
+test5_Gray_sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize = 5)
+abs_test5_Gray_sobelx = np.absolute(test5_Gray_sobelx) # Absolute x derivative to accentuate lines away from horizontal
+scaled_test5_Gray_sobelx = np.uint8(255*abs_test5_Gray_sobelx/np.max(abs_test5_Gray_sobelx))
 
-f, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize = (20,10))
+f, (ax0, ax1, ax2, ax3) = plt.subplots(1, 4, figsize = (20,10))
 ax0.imshow(test5_H_sobelx, cmap='gray')
 ax0.set_title('test5 Hue Sobel', fontsize = 20)
 ax1.imshow(test5_L_sobelx, cmap='gray')
 ax1.set_title('test5 Lightness Sobel', fontsize = 20)
 ax2.imshow(test5_S_sobelx, cmap='gray')
 ax2.set_title('test5 Saturation Sobel', fontsize = 20)
+ax3.imshow(test5_Gray_sobelx, cmap='gray')
+ax3.set_title('test5 Gray Sobel', fontsize = 20)
 
-f, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize = (20,10))
+f, (ax0, ax1, ax2, ax3) = plt.subplots(1, 4, figsize = (20,10))
 ax0.imshow(scaled_test5_H_sobelx, cmap='gray')
 ax0.set_title('test5 Hue Sobel Abs', fontsize = 20)
 ax1.imshow(scaled_test5_L_sobelx, cmap='gray')
 ax1.set_title('test5 Lightness Sobel Abs', fontsize = 20)
 ax2.imshow(scaled_test5_S_sobelx, cmap='gray')
 ax2.set_title('test5 Saturation Sobel Abs', fontsize = 20)
+ax3.imshow(scaled_test5_Gray_sobelx, cmap='gray')
+ax3.set_title('test5 Gray Sobel Abs', fontsize = 20)
 
 
-# In[82]:
+# The results above show that thresholding on the Lightness/Gray and Saturation channels would give us the best benefit.  The Hue channel is not usable in shadow regions.
+
+# ## Detect lane pixels 
+
+# In[32]:
 
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-#image = mpimg.imread('bridge_shadow.jpg')
-
 # Edit this function to create your own pipeline.
-def pipeline(img, s_thresh=(180, 240), sx_thresh=(25, 145)):
+def pipeline(img, s_thresh=(175, 245), sx_thresh=(25, 145)):
     img = np.copy(img)
     # Convert to HLS color space and separate the L, S channel
     hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
@@ -228,34 +242,152 @@ def pipeline(img, s_thresh=(180, 240), sx_thresh=(25, 145)):
 img_str8_warped_binary = pipeline(img_str8_warped)
 
 # Plot the result
-f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(24, 9))
-f.tight_layout()
+f, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(20,4))
 
 ax1.imshow(img_str8_warped)
-ax1.set_title('straight_lines1.jpg warped', fontsize = 20)
+ax1.set_title('straight_lines1.jpg warped', fontsize = 10)
 ax2.imshow(img_str8_warped_binary, cmap='gray')
-ax2.set_title('straight_lines1.jpg after gradient thresholding', fontsize = 20)
-ax3.plot(np.sum(img_str8_warped_binary[img_str8_warped_binary.shape[0]/2:,:], axis=0))
-ax3.set_title('straight_lines1.jpg histogram', fontsize = 20)
-
-plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+ax2.set_title('straight_lines1.jpg after gradient thresholding', fontsize = 10)
+ax3.plot(np.sum(img_str8_warped_binary[:int(img_str8_warped_binary.shape[0]/2),:], axis=0))
+ax3.set_title('test5.jpg top half histogram', fontsize = 10)
+ax4.plot(np.sum(img_str8_warped_binary[int(img_str8_warped_binary.shape[0]/2):,:], axis=0))
+ax4.set_title('test5.jpg bottom half histogram', fontsize = 10)
     
 img_test5_warped_binary = pipeline(img_test5_warped)
 
 # Plot the result
-f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(24, 9))
-f.tight_layout()
+f, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(20,4))
 
 ax1.imshow(img_test5_warped)
-ax1.set_title('test5.jpg warped', fontsize = 20)
+ax1.set_title('test5.jpg warped', fontsize = 10)
 ax2.imshow(img_test5_warped_binary, cmap='gray')
-ax2.set_title('test5.jpg after gradient thresholding', fontsize = 20)
-ax3.plot(np.sum(img_test5_warped_binary[img_test5_warped_binary.shape[0]/2:,:], axis=0))
+ax2.set_title('test5.jpg after gradient thresholding', fontsize = 10)
+ax3.plot(np.sum(img_test5_warped_binary[:int(img_test5_warped_binary.shape[0]/2),:], axis=0))
+ax3.set_title('test5.jpg top half histogram', fontsize = 10)
+ax4.plot(np.sum(img_test5_warped_binary[int(img_test5_warped_binary.shape[0]/2):,:], axis=0))
+ax4.set_title('test5.jpg bottom half histogram', fontsize = 10)
 
-ax3.set_title('test5.jpg histogram', fontsize = 20)
 
-plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+# The results of our image processing show us that for some difficult frames, portions of the lane will not be correctly detected unless a smart algorithm is put into place.  For instance, it could start at the bottom of the frame to find, follow and predict the curvature for higher parts of the frame, disregarding any other peaks.  Another way is if the right lane is definately identified, to predict where the left lane should be - since the distance between the lane lines is known.
 
+# ## Determine Lane Curvature and Vehicle position with respect to center
+
+# In[51]:
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+def scan_for_lanes(binary_warped):
+    # Assuming you have created a warped binary image called "binary_warped"
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(binary_warped[int(binary_warped.shape[0]/2):,:], axis=0)
+    # Create an output image to draw on and  visualize the result
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0]/2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+    
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set height of windows
+    window_height = np.int(binary_warped.shape[0]/nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+    
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = binary_warped.shape[0] - (window+1)*window_height
+        win_y_high = binary_warped.shape[0] - window*window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2) 
+        cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2) 
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:        
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+    
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+    
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds] 
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds] 
+    
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+    
+    return left_fit, right_fit, out_img
+
+
+# In[52]:
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# plot function
+def plot_lanes_on_warped(left_fit, right_fit, out_img):
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, out_img.shape[0]-1, out_img.shape[0] )
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+    
+    # color left lane red
+    #out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    # color right lane blue
+    #out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+    plt.imshow(out_img)
+    plt.plot(left_fitx, ploty, color='yellow')
+    plt.plot(right_fitx, ploty, color='yellow')
+    plt.xlim(0, 1280)
+    plt.ylim(720, 0)
+
+
+# In[53]:
+
+str8_left_fit, str8_right_fit, str8_out_img = scan_for_lanes(img_str8_warped_binary)
+plot_lanes_on_warped(str8_left_fit, str8_right_fit, str8_out_img)
+
+
+# The polynomial fitting on clean images works great.
+
+# In[54]:
+
+test5_left_fit, test5_right_fit, test5_out_img = scan_for_lanes(img_test5_warped_binary)
+plot_lanes_on_warped(test5_left_fit, test5_right_fit, test5_out_img)
+
+
+# On the shadowy image, the left lane isnt calculated accurately and results in an extra bend
 
 # In[ ]:
 
