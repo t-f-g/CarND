335a1c7e264fbe17d367325dd5cfa4b455e2d061
t-f-g <t-f-g@users.noreply.github.com>
2017-09-20 16:33:31 -0400

    Updated to final state of walkthrough

--- CarND-Path-Planning-Project/src/main.cpp
+++ CarND-Path-Planning-Project/src/main.cpp
@@ -201,7 +201,7 @@ int main() {
   int lane = 1;
 
   // Have a reference velocity to target
-  double ref_vel = 49.5; //mph
+  double ref_vel = 0.0; //mph
 
   h.onMessage([&ref_vel,map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
@@ -242,7 +242,49 @@ int main() {
 
             int prev_size = previous_path_x.size();
 
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+          	if(prev_size > 0)
+            {
+              car_s = end_path_s;
+            }
+
+            bool too_close = false;
+
+            //find ref_v to use
+            for(int i = 0; i < sensor_fusion.size(); i++)
+            {
+              //car is in my lane
+              float d = sensor_fusion[i][6];
+              if(d < (2+4*lane+2) && d > (2+4*lane-2) )
+              {
+                double vx = sensor_fusion[i][3];
+                double vy = sensor_fusion[i][4];
+                double check_speed = sqrt(vx*vx+vy*vy);
+                double check_car_s = sensor_fusion[i][5];
+
+                check_car_s+=((double)prev_size*.02*check_speed); //if using previous points can project s value out
+                //check s values greater than mine and s gap
+                if ((check_car_s > car_s) && ((check_car_s-car_s) < 30) )
+                {
+                  // Do some logic here, lower reference velocity so we dont crash into the car infront of us, could
+                  // also flag to try to change lanes.
+                  //ref_vel = 29.5; //mph
+                  too_close = true;
+                  if(lane > 0) // Add to finite state machine, plus cost functions
+                  {
+                    lane = 0;
+                  }
+                }
+              }
+            }
+
+            if(too_close)
+            {
+              ref_vel -= .224;
+            }
+            else if(ref_vel < 49.5)
+            {
+              ref_vel += .224;
+            }
 
             // Create a list of widely spaced (x,y) waypoints, evenly spaced at 30m
             // Later we will interpolate these waypoints with a spline and fill it in with more points that contrl speed
